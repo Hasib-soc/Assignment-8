17f400d8d4d09defe1f7e61112448ef712038647
Hasib-soc <hasib.soc@gmail.com>
2026-05-04 21:09:06 +0600

    additional information added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-"use client";
+
+
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function Home() {
   return (
-    <div className="text-center p-20">
+    <div className="text-center p-10">
       <h1 className="text-4xl font-bold">
         Discover Your Perfect Aesthetic
       </h1>
@@ -15,6 +18,20 @@ export default function Home() {
       <marquee className="mt-10">
         New Arrivals | Weekly Feature | Modern Tiles Collection
       </marquee>
+
+      <div className="mt-10">
+        <Swiper spaceBetween={10} slidesPerView={2}>
+          <SwiperSlide>
+            <img src="https://picsum.photos/400/300?1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://picsum.photos/400/300?2" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="https://picsum.photos/400/300?3" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
